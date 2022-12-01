8a5f7953393e145892f70447a7735a6a19334ee0
Gabriela Martinez <gabrielamar26@yahoo.es>
2022-12-01 18:19:10 -0500

    Format Ejercicio 13

--- src/Guia6/EjercicioExtra13.java
+++ src/Guia6/EjercicioExtra13.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+Crear un programa que dibuje una escalera de números, donde cada línea de números
+comience en uno y termine en el número de la línea. Solicitar la altura de la escalera al
+usuario al comenzar
  */
 package Guia6;
 
